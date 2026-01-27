b1ad19eb4dafb972a0d2e1fcb93ce7bd7854f71d
OchoKOM <martinkababo@gmail.com>
2026-01-27 23:47:05 +0100

    t

--- client/discovery.py
+++ client/discovery.py
@@ -1,5 +1,27 @@
 import socket
 import json
+from agent import get_mac, load_config
+
+DISCOVERY_PORT = 37020
+
+def discovery_listener():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.bind(("", DISCOVERY_PORT))
+
+    print("Écoute discovery UDP...")
+
+    while True:
+        data, addr = sock.recvfrom(1024)
+        if data.decode() == "DISCOVER_REMOTE_AGENT":
+            config = load_config()
+            response = {
+                "name": config["name"],
+                "mac": get_mac(),
+                "port": 5000
+            }
+            sock.sendto(json.dumps(response).encode(), addr)
+
 
 DISCOVERY_PORT = 37020
 MESSAGE = "DISCOVER_REMOTE_AGENT"
